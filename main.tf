61a237d6bcda81f7cecd01be57a92873c2280665
Kevin Mahoney <716806+kevcube@users.noreply.github.com>
2022-03-02 14:23:18 -0500

    fix version

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 data "aws_region" "current" {}
 
 module "ecr" {
-  source  = "git@github.com:kevcube/terraform-aws-ecs-codepipeline.git"
-  version = "be3242c"
+  source  = "git@github.com:kevcube/terraform-aws-ecs-codepipeline.git?ref=be3242c"
   enabled = var.codepipeline_enabled
 
   attributes           = ["ecr"]
